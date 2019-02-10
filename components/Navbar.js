11f2ee1234458c4a4846a74f2a47a29ebe7e9f3b
Anthony Ioimo <anthonylioimo@gmail.com>
2019-02-10 21:35:56 +0100

    update bootstrap navbar, add container

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,33 +1,21 @@
 import Link from 'next/link';
 
 const Navbar = () => (
-  <div>
-    <ul>
-      <li><Link href="/"><a>Home</a></Link></li>
-      <li><Link href="/about"><a>About</a></Link></li>
-    </ul>
-
-    <style jsx>{`
-      ul {
-        background: #333;
-        color: #fff;
-        list-style: none;
-        display: flex;
-
-      }
-
-      ul li {
-        font-size: 18px;
-        margin-right: 20px;
-      }
-
-      ul li a {
-        color: #fff;
-        text-decoratoin: none;
-      }
-
-    `}</style>
-  </div>
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+    <div className="container">
+      <a className="navbar-brand" href="#">BitzPrice</a>
+      <div className="collapse navbar-collapse" id="navbarColor02">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link href="/"><a className="nav-link">Home</a></Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/about"><a className="nav-link">About</a></Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
 );
 
