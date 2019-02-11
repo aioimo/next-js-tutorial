88dd34ffcfbb12ae70ab04656173550c42afcce2
Anthony Ioimo <anthonylioimo@gmail.com>
2019-02-11 10:59:23 +0100

    configure state to change currencies

--- components/Prices.js
+++ components/Prices.js
@@ -3,10 +3,29 @@ class Prices extends React.Component {
     currency: 'USD'
   }
 
+  changeCurrency = (e) => {
+    this.setState({
+      currency: e.target.value
+    })
+  }
+
   render() {
+    let {currency} = this.state;
+   
     return (
       <div>
-        Prices
+        <ul className="list-group">
+          <li className="list-group-item">
+            Bitcoin Rate for {this.props.bpi[currency].description} : <span class="badge badge-primary">{this.props.bpi[currency].code}</span>
+            <strong>{this.props.bpi[currency].rate}</strong>
+          </li>
+        </ul>
+        <br/>
+        <select onChange={this.changeCurrency} className="form-control">
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="GBP">GBP</option>
+        </select>
       </div>
     )
   }
